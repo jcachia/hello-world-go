f0088f24cf261db3602695d501e1b004eb9d6391
Jason C <jason.cachia@care.com>
2023-01-16 10:45:39 -0500

    more vars

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import "fmt"
 func main() {
 	// Prints, then inserts line break
 	// fmt.Println("Hello, world!")
+
 	// Prints both of these without a line break
 	// fmt.Print("This is some text")
 	// fmt.Print("this is some more text")
+
 	// This is how to pass an argument to a func
-	sayHelloWorld(("Passing a string, as an argument to a func"))
+	// sayHelloWorld(("Passing a string, as an argument to a func"))
+
+	// Instead using a variable argument
+	// var whatToSay string
+	// whatToSay = "string from a var"
+	// sayHelloWorld((whatToSay))
+
+	// variable shorthand, assignment operator.
+	whatToSay := "create and store a var in one line, and determine its type automatically"
+	sayHelloWorld((whatToSay))
+
 }
 
 // Passing arguments to function.  Must list arg name and type.
