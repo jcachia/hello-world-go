73e1fa9356eeb698f55f3d7d4d4371f75a846c8f
Jason C <jason.cachia@care.com>
2023-01-16 10:34:43 -0500

    arguments in funcs

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Prints, then inserts line break
+	// fmt.Println("Hello, world!")
+	// Prints both of these without a line break
+	// fmt.Print("This is some text")
+	// fmt.Print("this is some more text")
+	// This is how to pass an argument to a func
+	sayHelloWorld(("Passing a string, as an argument to a func"))
+}
+
+// Passing arguments to function.  Must list arg name and type.
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
